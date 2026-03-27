537f050904ae9c4664dee314ea1e60c2bd15aec7
Alan_dan <aayalaocampo@uade.edu.ar>
2026-03-27 16:03:06 -0300

    se agrego info

---
+++ src/info.py
@@ -0,0 +1,11 @@
+
+datosagencia = ["""CENTRAL DE PASAJES DE MICRO""", """LEGAJO - 115758""", """AV. RAMOS MEJIA 1560""", """TEL: 11-6824-8705""",
+                """MAIL:centraldemicro10@gmail.com"""]
+for i in range(len(datosagencia)):
+    print(datosagencia[i])
+        
+print("""\nDESTINOS Y PUNTO DE PARTIDA DISPONIBLES:""")
+datosdestinos=["""BARILOCHE""","""MAR DEL PLATA""","""MISIONES""", """BUENOS AIRES"""]
+datosdestinos.sort()
+for i in range(len(datosdestinos)):
+    print(datosdestinos[i])
