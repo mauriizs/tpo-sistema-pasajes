3af056da1b17c272122641d94e979c39d1223fec
Mauri <mfarfanflorez@uade.edu.ar>
2026-04-08 20:02:33 -0300

    Implement fecha validation function and remove outdated validar-fechas.py

--- src/f_validaciones.py
+++ src/f_validaciones.py
@@ -1,5 +1,15 @@
 import re
 
+def validar_fecha(fecha_ingresada): # Valida que la fecha tenga el formato dd/mm y que los valores sean válidos.  
+    
+    if re.match(r'^\d{2}/\d{2}$', fecha_ingresada):
+        dia, mes = map(int, fecha_ingresada.split('/'))
+        if 1 <= dia <= 31 and 1 <= mes <= 12:
+            return True
+
+
+
+
 def validar_dni(dni):
     # TODO: Usar re.match() para validar que sean números
     pass
