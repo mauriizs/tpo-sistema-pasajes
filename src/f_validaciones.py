db40f6f41f8d3ec8c4a8b51c534c682588d5f86d
Mauri <mfarfanflorez@uade.edu.ar>
2026-04-09 17:56:15 -0300

    Implementacion de validaciones (fecha, dni, email y telefono)

--- src/f_validaciones.py
+++ src/f_validaciones.py
@@ -7,14 +7,28 @@ def validar_fecha(fecha_ingresada): # Valida que la fecha tenga el formato dd/mm
             if 1 <= dia <= 31 and 1 <= mes <= 12:
                 return fecha_ingresada
         print("Fecha no válida. Ingresá en formato DD/MM.")
-        fecha_ingresada = input("Ingresá la fecha del viaje (DD/MM): ")
+        fecha_ingresada = input("> Ingresá la fecha del viaje (DD/MM): ")
 
+def validar_dni(dni_ingresado):
+    while True:
+        # RegEx: Exactamente entre 7 y 8 dígitos numéricos
+        if re.match(r'^\d{7,8}$', dni_ingresado):
+            return dni_ingresado
+        print("[ERROR] El DNI debe contener solo números (7 u 8 dígitos).")
+        dni_ingresado = input("> Ingresá tu DNI (sin puntos): ")
 
-def validar_dni(dni):
-    # TODO: Usar re.match() para validar que sean números
-    pass
+def validar_email(email_ingresado):
+    while True:
+        # RegEx: Formato basico de email texto@texto.texto
+        if re.match(r'^[\w\.-]+@[\w\.-]+\.\w+$', email_ingresado):
+            return email_ingresado
+        print("[ERROR] Formato de email no válido.")
+        email_ingresado = input("> Ingresá tu Email: ")
 
-def validar_email(email):
-    # TODO: Usar re.match() para validar el formato correo@dominio.com
-    pass
-
+def validar_telefono(tel_ingresado):
+    while True:
+        # RegEx: Solo números, entre 8 y 15 dígitos
+        if re.match(r'^\d{8,15}$', tel_ingresado):
+            return tel_ingresado
+        print("[ERROR] Teléfono no válido (solo números).")
+        tel_ingresado = input("> Ingresá tu Teléfono: ")
